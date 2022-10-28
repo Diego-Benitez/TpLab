d8033c9783612c315c9104b7e3e660c61e8a25a7
Diego Ezequiel Benitez <104686275+Diego-Benitez@users.noreply.github.com>
2022-10-28 13:49:50 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,68 @@
 
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+
+#include "funciones.h"
+#include "partida.h"
+#include "rlutil.h"
+
+using namespace std;
+
+void jA1();
+void punto5();
+void J1();
+
+// void cargarVector();
+
+int main() {
+
+  bool repite = true;
+  int opc;
+  while (true) {
+    rlutil::setBackgroundColor(rlutil::GREEN);
+    rlutil::cls();
+    rlutil::locate(30, 10);
+    cout << "---------MENU----------" << endl;
+    rlutil::locate(30, 11);
+    cout << "1. JUEGO NUEVO DE A 1" << endl;
+    rlutil::locate(30, 12);
+    cout << "2. JUEGO NUEVO DE A 2" << endl;
+    rlutil::locate(30, 13);
+    cout << "3. MOSTRAR PUNTUACION TOTAL" << endl;
+    rlutil::locate(30, 14);
+    cout << "4. MODO TESTEO" << endl;
+    rlutil::locate(30, 15);
+    cout << "0. SALIR" << endl;
+    rlutil::locate(30, 16);
+    cout << "-----------------------" << endl;
+    rlutil::locate(30, 17);
+    cout << "OPCION: ";
+    cin >> opc;
+    rlutil::cls();
+    switch (opc) {
+    case 1:
+      J1();
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      punto5();
+
+      system("pause");
+      break;
+    case 0:
+      cout << "ADIOS!!" << endl;
+      return 0;
+      repite = false;
+      break;
+    }
+    repite = true;
+
+    system("pause");
+  }
+  return 0;
